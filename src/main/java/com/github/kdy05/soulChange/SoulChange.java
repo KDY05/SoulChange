4dbc4df290942200c231866d2db041d8cc05c630
kdy05 <kwondy0500@naver.com>
2025-01-07 23:16:29 +0900

    first commit

---
+++ src/main/java/com/github/kdy05/soulChange/SoulChange.java
@@ -0,0 +1,17 @@
+package com.github.kdy05.soulChange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoulChange extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
